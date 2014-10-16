33758ba9906394a965999c0e9842cc04a37bd337
Fin <finwood@posteo.de>
2014-10-16 12:57:55 +0200

    gyro.py now converts the logs into csv

--- src/gyro.py
+++ src/gyro.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python2.7
 # -*- codung: utf-8 -*-
 
-with open('gyro.log') as f:
-	for line in f:
-		x = int(line)
-		print x
+from sys import argv
+import re
+
+# [2014-10-16 12:13:32.311] C: -22
+preg = re.compile('\[([^\]]+)\] (0|C): (-?\d+)')
+print ';'.join(["'%s'" %item for item in ('time', 'status', 'value')])
+
+for line in open(argv[1]):
+	m = preg.match(line)
+	if m:
+		print ';'.join(["'%s'" %item for item in m.groups()])
 
