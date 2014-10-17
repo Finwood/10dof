638dbcd6398df12ff4f1bd8201551de38c818cd4
Fin <finwood@posteo.de>
2014-10-17 14:50:54 +0200

    added high pass filter, modified gyro.py to automatically display measured data graphically

--- src/gyro.py
+++ src/gyro.py
@@ -5,20 +5,28 @@ from pandas import DataFrame, Series
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 from sys import argv
-path = argv[1]
 
 def hex2int(s):
 	return int(s, 16)
 
-from datetime import datetime as dt
 def ts2dt(s):
 	return dt.fromtimestamp(float(s))
 
-df = pd.read_csv(path, skiprows=50, names=['time', 'status', 'value'], index_col=0, converters={'time': ts2dt, 'status': hex2int})
-# do something with status
-cs = df.drop('status', axis=1).cumsum(0)
-plt.figure()
-cs.plot()
-plt.show()
+for p in argv[1:]:
+	fig, axes = plt.subplots(2, 1)
+	df = pd.read_csv(p, index_col=0, converters={'tstamp': ts2dt, 'status': hex2int})
+	if (df.status != 0x0F).sum() > 0:
+		print "irgendetwas stimmt mit den Stati nicht, bitte guck selbst nach."
+	else:
+		print p
+		df.drop('status', axis=1, inplace=True)
+		(df * 8.75e-5).cumsum().cumsum().plot(ax=axes[0], title=p)
+		((df.cumsum() - df[:500].cumsum().mean()) * 8.75e-5).cumsum().plot(ax=axes[1])
+#		(df * 8.75e-5).cumsum().plot(ax=axes[1])
+#		((df - df[:100].mean()) * 8.75e-5).cumsum().plot(ax=axes[1])
+		fig.show()
+
+raw_input()
