068d612dbf2992ca57570d6f3b3ddfef58b7f738
Fin <finwood@posteo.de>
2014-11-01 00:20:43 +0100

    added pitch, quick'n'dirty

--- src/gui/horizon.py
+++ src/gui/horizon.py
@@ -7,6 +7,7 @@ try:
 except:
 	import Tkinter as tk
 from PIL import Image, ImageTk
+from math import sin, cos, pi
 
 STEP = 5
 
@@ -15,8 +16,10 @@ class Horizon(tk.Canvas):
 		self.app = app
 		tk.Canvas.__init__(self, app, width=280, height=280)
 
-		self.angle = 0
-		self.steps = 0
+		self.roll = 0
+		self.droll = 0
+		self.pitch = 30
+		self.dpitch = 0
 
 		self.img_horizon = Image.open('img/horizon.png')
 		self.img_frame = Image.open('img/frame.png')
@@ -25,23 +28,26 @@ class Horizon(tk.Canvas):
 		self.tkimg_horizon = ImageTk.PhotoImage(self.img_horizon)
 		self.tkimg_frame = ImageTk.PhotoImage(self.img_frame)
 		self.tkimg_scale = ImageTk.PhotoImage(self.img_scale)
-		self.create_image(140, 180, image=self.tkimg_horizon, tag='Horizon')
+		self.create_image(140, 140, image=self.tkimg_horizon, tag='Horizon')
 		self.create_image(140, 140, image=self.tkimg_frame, tag='Frame')
 		self.create_image(140, 140, image=self.tkimg_scale, tag='Scale')
 
-	def rot_step(self, step):
-		self.steps += step
-		self.angle = self.steps%360
+	def roll_step(self, step):
+		self.droll += step
+		self.roll = self.droll%360
+		self.redraw()
 
-		self.tkimg_horizon = ImageTk.PhotoImage(self.img_horizon.rotate(self.angle, resample=Image.BICUBIC))
+	def redraw(self):
+		self.tkimg_horizon = ImageTk.PhotoImage(self.img_horizon.rotate(self.roll, resample=Image.BICUBIC))
 		self.itemconfigure('Horizon', image=self.tkimg_horizon)
-		self.coords('Horizon', 120, 140) # läuft!
-		self.tkimg_scale = ImageTk.PhotoImage(self.img_scale.rotate(self.angle, resample=Image.BICUBIC))
+		self.coords('Horizon', 140 + self.pitch * sin(self.roll / 180.0 * pi), 140 + self.pitch * cos(self.roll / 180.0 * pi)) # läuft!
+
+		self.tkimg_scale = ImageTk.PhotoImage(self.img_scale.rotate(self.roll, resample=Image.BICUBIC))
 		self.itemconfigure('Scale', image=self.tkimg_scale)
 
 def inc():
 	global horizon, app_win
-	horizon.rot_step(-1)
+	horizon.roll_step(-1)
 	app_win.after(100, inc)
 
 def main():
@@ -53,9 +59,9 @@ def main():
 	button_frame = tk.Frame(app_win)
 
 	tk.Button(button_frame, text="--->", height=3,
-		command=partial(horizon.rot_step, STEP)).pack(fill='x')
+		command=partial(horizon.roll_step, STEP)).pack(fill='x')
 	tk.Button(button_frame, text="<---", height=3,
-		command=partial(horizon.rot_step, -STEP)).pack(fill='x')
+		command=partial(horizon.roll_step, -STEP)).pack(fill='x')
 	tk.Button(button_frame, text="Beenden", width=40, height=3,
 		command=app_win.destroy).pack()
 
